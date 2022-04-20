5263e146fcef3473987409ba2d7cad2df45d1b8d
Euclid-Twin <wdashuang@gmail.com>
2022-04-20 14:21:19 +0800

    update extension verison

--- src/pages/options/About/index.tsx
+++ src/pages/options/About/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './index.less';
+import packageJson from '../../../../package.json';
+
 export default () => {
   return (
     <div className="common-container">
       <h2 className="page-title">About</h2>
-      <p>Soda extension v0.2.10.</p>
+      <p>Soda extension v{packageJson.version}.</p>
       <p>©Sonet Team. All rights Reserved.</p>
     </div>
   );
